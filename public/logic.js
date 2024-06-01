b3e48841fd32e2f06f6f8736443da905f3205d1f
DamianKobylinski <damiankob12@gmail.com>
2024-06-01 13:34:03 +0200

    delete unnecessary sendMessage

--- public/logic.js
+++ public/logic.js
@@ -2,9 +2,3 @@ document.addEventListener('click', function (e) {
     e.preventDefault();
     chrome.runtime.sendMessage({action:"open_loading_site",data: e.target.href});
 });
-
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-    if (request.action === 'updateData') {
-        console.log('Received data:', request.data);
-    }
-});
