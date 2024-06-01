17d401cfa8a64f0fe1d4f1ddd22fe11503ed9f1a
kosiarn <62908964+kosiarn@users.noreply.github.com>
2024-06-01 12:37:36 +0200

    switch root component in loading.html

--- src/loading.tsx
+++ src/loading.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import Load from './test/load.tsx'
+//import Load from './test/load.tsx'
+import LoadingScreen from './views/LoadingScreen'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <Load />
+        <LoadingScreen />
     </React.StrictMode>,
 )
