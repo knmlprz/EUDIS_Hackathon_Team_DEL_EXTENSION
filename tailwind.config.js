f1b19843961971f585fe068083e5000833703c74
kosiarn <62908964+kosiarn@users.noreply.github.com>
2024-06-01 15:11:37 +0200

    add fonts to Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        'display': ['Arial'],
+        'main': ['Fira Sans']
+      },
       colors: {
         neutrals: {
           black: "#030408",
