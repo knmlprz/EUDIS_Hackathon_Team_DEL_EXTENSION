eef9f9ea81b7a17f4c966c4eca0f713421aeb07c
kosiarn <62908964+kosiarn@users.noreply.github.com>
2024-06-01 13:32:37 +0200

    add scan fetching to loading screen view

--- src/views/LoadingScreen.tsx
+++ src/views/LoadingScreen.tsx
@@ -1,16 +1,26 @@
 import logo from '../assets/logo.svg'
 import LoadingIcon from '../components/LoadingIcon'
+import {requestScan, scanResponse} from '../api/api'
+import { useEffect, useState } from 'react'
 
 export default function LoadingScreen(){
     const queryParameters = new URLSearchParams(window.location.search)
     const url = queryParameters.get("data")
-    if(url){
-        console.log(`Detected URL: ${url}`)
-    }
+    const [scanResult, setScanResult] = useState<scanResponse | null>(null);
+    useEffect(() => {
+        async function fetchScan(){
+            if(url){
+            setScanResult(await requestScan(url))
+            }
+        }
+        fetchScan()
+    }, [])
+
     return (
     <div className="flex flex-col w-max h-max bg-dark_blue-700">
         <img src={logo} className="w-[280px]"/>
         <LoadingIcon />
+        {scanResult?.safe? 'bruhnot' : 'bruh'}
     </div>
     )
 }
