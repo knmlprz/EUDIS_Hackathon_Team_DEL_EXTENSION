fcaef6454a5f86399566c1db71b92031e5617385
kosiarn <62908964+kosiarn@users.noreply.github.com>
2024-06-01 15:24:41 +0200

    make eslint ignore empty dependecy array in useEffect in LoadingScreen.jsx

--- src/views/LoadingScreen.tsx
+++ src/views/LoadingScreen.tsx
@@ -23,6 +23,7 @@ export default function LoadingScreen(){
             fetchScan(url);
             }
         },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
         []
     )
 
