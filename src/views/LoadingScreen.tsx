f1172b924bf034c24d5ca9c6bf6845ecdf165b53
kosiarn <62908964+kosiarn@users.noreply.github.com>
2024-06-01 12:47:38 +0200

    make LoadingScreen detect `url` GET parameter

--- src/views/LoadingScreen.tsx
+++ src/views/LoadingScreen.tsx
@@ -3,12 +3,14 @@ import LoadingIcon from '../components/LoadingIcon'
 
 export default function LoadingScreen(){
     const queryParameters = new URLSearchParams(window.location.search)
-    const name = queryParameters.get("name")
+    const url = queryParameters.get("url")
+    if(url){
+        console.log(`Detected URL: ${url}`)
+    }
     return (
     <div className="flex flex-col w-max h-max bg-dark_blue-700">
         <img src={logo} className="w-[280px]"/>
         <LoadingIcon />
-        <p className='font-bold text-9xl text-neutrals-white'>{name ? name : ""}</p>
     </div>
     )
 }
