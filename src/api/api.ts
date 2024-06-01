25c42f3daefea78ba6366ec655858b915c500379
kosiarn <62908964+kosiarn@users.noreply.github.com>
2024-06-01 15:12:34 +0200

    update api.ts

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,18 +1,20 @@
 import axios from "axios";
 
 export type scanResponse = {
-    safe: boolean
+    safe: boolean | undefined
 }
+
+
+export type APIScanResponse = scanResponse
 
 export default function API(){
-    return axios.create({baseURL: 'http://localhost:8000'})
+    return axios.create({baseURL: 'http://localhost:8000', timeout: 3000})
 }
 
-export async function requestScan(resource_url: string){
-    const response = await API().post(`/scan/`,{data: resource_url})
-    return response.data as scanResponse
-}
-
-export function isUndefined(response: scanResponse): boolean {
-    return response.safe === undefined
+export async function RequestScan(resource_url: string): Promise<APIScanResponse>{
+    return new Promise<APIScanResponse>((resolve) => {
+        API().post(`/scan/`,{data: resource_url}).catch(() => {
+            resolve({safe: undefined})
+        }).then((response) => resolve(response?.data))
+    }) 
 }
