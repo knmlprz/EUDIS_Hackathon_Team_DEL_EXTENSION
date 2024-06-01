167a0e789d35da812e346c825250f2bcbb5cec1a
DamianKobylinski <damiankob12@gmail.com>
2024-06-01 12:55:42 +0200

    send get link to check

--- public/background.js
+++ public/background.js
@@ -2,8 +2,10 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === "open_loading_site") {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             let activeTab = tabs[0];
-            chrome.tabs.update(activeTab.id, { url: chrome.runtime.getURL("loading.html") });
+            chrome.tabs.update(activeTab.id, { url: chrome.runtime.getURL(`loading.html?data=${encodeURIComponent(request.data)}`)
+            });
         });
+
     }
 });
 
@@ -13,7 +15,7 @@ let pollInterval = 5000;
 function fetchData() {
     chrome.tabs.query({}, function(tabs) {
         tabs.forEach(tab => {
-            chrome.tabs.sendMessage(tab.id, { action: 'updateData', data: "Fetching data..." });
+            chrome.tabs.sendMessage(tab.id, { action: 'updateData', data: { message: 'Hello from background.js' }});
         });
     });
 }
