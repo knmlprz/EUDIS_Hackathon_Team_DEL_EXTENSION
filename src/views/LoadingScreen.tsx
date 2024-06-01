47b730f0081d66d37984638a9ab76473efb49d3f
kosiarn <62908964+kosiarn@users.noreply.github.com>
2024-06-01 12:37:19 +0200

    improve loading screen add animated loading icon and reading GET parameter

--- src/views/LoadingScreen.tsx
+++ src/views/LoadingScreen.tsx
@@ -1,10 +1,14 @@
 import logo from '../assets/logo.svg'
+import LoadingIcon from '../components/LoadingIcon'
 
 export default function LoadingScreen(){
+    const queryParameters = new URLSearchParams(window.location.search)
+    const name = queryParameters.get("name")
     return (
-    <div>
+    <div className="flex flex-col w-max h-max bg-dark_blue-700">
         <img src={logo} className="w-[280px]"/>
-
+        <LoadingIcon />
+        <p className='font-bold text-9xl text-neutrals-white'>{name ? name : ""}</p>
     </div>
     )
 }
